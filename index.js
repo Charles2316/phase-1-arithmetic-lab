aa63675db2b97c53f13f959ae337462ac1bf534e
Charles <charlessmith2316@gmail.com>
2023-02-27 08:32:47 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 62
+let num2 = 1 
+ let multiply = num1 * num2 
 
-// Write your code here
+
+ let random
+ random = Math.floor(Math.random() +1 ) 
+
+
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4
+
+let max = Math.max(2,10,20)
